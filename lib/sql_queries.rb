82720a0fa232fb3497bbae5e88a35d490c384383
zachary mines <zmines@wesleyan.edu>
2020-08-18 21:52:55 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,13 +29,14 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "Write your SQL query Here"
+  "SELECT users.name, SUM(pledges.amount)
+  FROM users, pledges WHERE pledges.user_id = users.id GROUP BY users.name ORDER BY SUM(pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Write your SQL query Here"
+  "SELECT projects.category, pledges.amount FROM projects, pledges WHERE projects.category = 'music' AND pledges.project_id = projects.id;"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "Write your SQL query Here"
+  "SELECT projects.category, SUM(pledges.amount) FROM projects, pledges WHERE projects.category = 'books' AND pledges.project_id = projects.id;"
 end
