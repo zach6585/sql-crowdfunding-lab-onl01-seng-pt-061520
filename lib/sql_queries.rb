b79b213a9148c2cf409deda95f232dc75d5e4647
zachary mines <zmines@wesleyan.edu>
2020-08-18 21:46:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,7 +34,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "SELECT projects.category, pledges.amount FROM projects, pledges WHERE projects.category = 'music' GROUP BY projects.title;"
+  "SELECT projects.category, pledges.amount FROM projects, pledges WHERE projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
